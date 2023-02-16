4acfda612fef16c2e6664834ce0b36b838327e6c
Danielikye <iwuegbulemdanielikechukwu@gmail.com>
2023-02-16 12:40:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * Return: Always returns (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
