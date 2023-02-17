567880048cc6eb0d44f4e7cfd85634aa51b7b575
Danielikye <iwuegbulemdanielikechukwu@gmail.com>
2023-02-17 15:03:02 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,19 @@ int main(void)
 {
 	char c;
 
+	char d;
+
 	c = 'a';
 	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
 		}
 	putchar('\n');
 	return (0);
