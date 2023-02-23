9e51e963844543dca87c4c525a193246476e3898
Danielikye <iwuegbulemdanielikechukwu@gmail.com>
2023-02-23 13:20:32 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
