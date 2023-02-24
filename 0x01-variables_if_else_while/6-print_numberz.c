1ba25bf64d09fc089737b669f85376b0819ea157
Danielikye <iwuegbulemdanielikechukwu@gmail.com>
2023-02-24 06:36:53 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int c;
+
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
