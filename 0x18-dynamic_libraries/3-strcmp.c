4b6aa6c206b16e32a69836df441312f452aaaa6a
danielikye <iwuegbulemdanielikechukwu@gmail.com>
2023-06-19 14:09:52 -0700

    updated

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare string values
+ * @s1: Input value
+ * @s2: Input value
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
